a2c60733e2af9ae5252b6a7d47c33407a03c2a46
David W. Hogg <dwhogg@gmail.com>
2012-03-20 09:38:54 -0400

    outline of the plan

---
+++ py/select_and_caption.py
@@ -0,0 +1,25 @@
+'''
+This file is part of the Sloan Atlas project.
+Copyright 2012 David W. Hogg (NYU).
+
+to-do:
+
+- Make plots of the input catalog (currently the NASA-Sloan Atlas) to
+  guide selection.
+
+- Design boxels in top-level space (color and central surface
+  brightness?).
+
+- Within each boxel, select galaxies in decreasing order in angular
+  size.
+
+- For each galaxy selected, produce "caption information" including
+  plots of location in property space (say, radial profile and
+  something?), RC3 classification, and GalaxyZoo information.
+
+- Build plates from galaxies based on angular size and build joint
+  captions for plates.
+'''
+
+if __name__ == '__main__':
+    print "Hello World"
