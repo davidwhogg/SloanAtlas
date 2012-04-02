78bb049f94a7e813400c4f2035aebca1eb6ac6d3
Ekta Patel <ep1091@nyu.edu>
2012-04-02 16:13:31 -0400

    plots for: radius distribution, RA vs DEC and iband flux vs radius

--- py/select_and_caption.py
+++ py/select_and_caption.py
@@ -23,13 +23,14 @@ to-do:
 '''
 
 import matplotlib
-matplotlib.use('Agg')
-from matplotlib import rc
-rc('font',**{'family':'serif','serif':'Computer Modern Roman','size':12})
-rc('text', usetex=True)
+#matplotlib.use('Agg')
+#from matplotlib import rc
+#rc('font',**{'family':'serif','serif':'Computer Modern Roman','size':12})
+#rc('text', usetex=True)
 import numpy as np
 import pylab as plt
 import pyfits as pyf
+
 
 if __name__ == '__main__':
     data = pyf.open("../data/nsa-short.fits.gz")[1].data
@@ -38,5 +39,30 @@ if __name__ == '__main__':
     plt.clf
     plt.hist(data.field('SERSIC_TH50'))
     plt.xlabel('half-light radius (arcsec)')
-    plt.savefig('radius_distribution.png')
+    plt.savefig('radius_distribution.png')    
+plt.show()  
+
+if __name__ == '__main__':
+    data = pyf.open("../data/nsa-short.fits.gz")[1].data
+    x=data.field('RA')
+    y=data.field('DEC')
+    plt.xlabel('Right Ascension')
+    plt.ylabel('Declination')
+    plt.savefig('RA_DEC.png')
+    plt.plot(x,y,'.m')  
+plt.show()  
+
+
+if __name__ == '__main__':
+    data = pyf.open("../data/nsa-short.fits.gz")[1].data
+    x=data.field('SERSIC_TH50')
+    y=data.field('SERSICFLUX')
+    for t in y:
+        i=t[5]
+    plt.ylabel('half-light radius (arcsec)')
+    plt.xlabel('Flux- i band')
+    plt.savefig('radius_iband.png')
+    plt.plot(y,x,'.c')
+    plt.ylim(30, 165)     
+plt.show()    
 
