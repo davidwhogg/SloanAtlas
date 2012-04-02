dd4d34fd86931aca7cfe722ca5860095bde15d9a
Ekta Patel <ep1091@nyu.edu>
2012-04-02 18:23:49 -0400

    created 4 separate figures rather than subplots

--- py/select_and_caption.py
+++ py/select_and_caption.py
@@ -32,6 +32,7 @@ import pylab as plt
 import pyfits as pyf
 
 
+fig1 = plt.figure(1)
 if __name__ == '__main__':
     data = pyf.open("../data/nsa-short.fits.gz")[1].data
     for t in data.field('SERSIC_TH50'):
@@ -40,8 +41,8 @@ if __name__ == '__main__':
     plt.hist(data.field('SERSIC_TH50'))
     plt.xlabel('half-light radius (arcsec)')
     plt.savefig('radius_distribution.png')    
-plt.show()  
-
+ 
+fig2 = plt.figure(2)
 if __name__ == '__main__':
     data = pyf.open("../data/nsa-short.fits.gz")[1].data
     x=data.field('RA')
@@ -49,12 +50,12 @@ if __name__ == '__main__':
     plt.xlabel('Right Ascension')
     plt.ylabel('Declination')
     plt.plot(x,y,'.m')
-    plt.savefig('RA_DEC.png')
     plt.xlim(-50,400)
+    plt.savefig('RA_DEC.png')
       
-plt.show()  
+  
 
-
+fig3 = plt.figure(3)
 if __name__ == '__main__':
     data = pyf.open("../data/nsa-short.fits.gz")[1].data
     x=data.field('SERSIC_TH50')
@@ -66,24 +67,30 @@ if __name__ == '__main__':
     plt.ylabel('half-light radius (arcsec)')
     plt.xlabel('Flux- i band')
     plt.plot(y,x,'.c')
+    plt.ylim(30, 165)  
     plt.savefig('radius_iband.png')
-    plt.ylim(30, 165)     
-plt.show()   
-
+  
+fig4 = plt.figure(4)
 if __name__ == '__main__':
     data = pyf.open("../data/nsa-short.fits.gz")[1].data
     x=data.field('SERSIC_TH50')
     y=data.field('SERSICFLUX')
-    def iMag(y):
+    def iMag1(y):
         for t in y:
-            g=t[5]
-        return 22.5-2.5*np.log10(np.abs(y))   
-    f=iMag(y)
+            t[5] < 0 
+        return 22.5-2.5*np.log10(np.abs(y))  
+    def iMag2(y):
+        for t in y:
+            t[5] > 0
+        return 22.5-2.5*np.log10(y)
+    f=iMag1(y)   
+    g=iMag2(y)
+    plt.plot(f,x,'.r')
+    plt.plot(g,x,'.b')
     plt.ylabel('half-light radius (arcsec)')
-    plt.xlabel('Magnitude- i band')
-    plt.plot(f,x,'.b')
-    plt.savefig('radius_magiband.png')    
+    plt.xlabel('Magnitude- i band')    
     plt.xlim(-20,140)
-    plt.ylim(20,165) 
+    plt.ylim(20,165)
+    plt.savefig('radius_magiband.png')
 plt.show()  
 
