dc05a73e5c6c88fc285af7a35710ea112841eb05
Ekta Patel <ep1091@nyu.edu>
2012-04-16 19:23:58 -0400

    all plots now contain 'bad' values

--- py/select_and_caption.py
+++ py/select_and_caption.py
@@ -36,17 +36,38 @@ import pyfits as pyf
 fig1 = plt.figure(1)
 if __name__ == '__main__':
     data = pyf.open("../data/nsa-short.fits.gz")[1].data
-    x=data.field('RA')
-    y=data.field('DEC')
+    a=data.field('RA')
+    b=data.field('DEC')
+    z = data.field('SERSIC_TH50')
+    y = data.field('SERSICFLUX')
+    good=np.array([True for x in data.field('RA')])
+    indx1=np.where(y[:,4] < 0)
+    good[indx1]=False
+    indx2=np.where(y[:,2] < 0)
+    good[indx2]=False
+    indx3=np.where(z > 158)
+    good[indx3]=False
+    
+    x=a[good]
+    x2=a[indx1]
+    x3=a[indx2]
+    x4=a[indx3]
+    y=b[good]
+    y2=b[indx1]
+    y3=b[indx2]
+    y4=b[indx3]
+
     plt.xlabel('Right Ascension')
     plt.ylabel('Declination')
     plt.plot(x,y,'ko', alpha=0.5)
+    plt.plot(x2, y2, 'mo', alpha=0.5)
+    plt.plot(x3,y3,'mo',alpha=0.5)
+    plt.plot(x4,y4,'mo',alpha=0.5)
     plt.xlim(-5,360)
     plt.ylim(-30,90)
     plt.savefig('RA_DEC.png')
   
 #scatter plot of radii vs. magnitude calculated using i-band flux
-
 fig2 = plt.figure(2)
 if __name__ == '__main__':
     data = pyf.open("../data/nsa-short.fits.gz")[1].data
@@ -74,7 +95,7 @@ if __name__ == '__main__':
     plt.ylabel('Half-Light Radius (arcsec)')
     plt.xlabel('I Magnitude')  
     plt.xlim(7,23)
-    plt.ylim(0,161)
+    plt.ylim(0,180)
     plt.savefig('radius_imag.png')
   
     
@@ -113,7 +134,7 @@ if __name__ == '__main__':
     b=z[indx1]
     c=z[indx2]
     d=z[indx3]
-
+    
     h=g-i
     n=m-j
     o=k-l
@@ -123,7 +144,7 @@ if __name__ == '__main__':
     plt.plot(o,c, 'mo', alpha=0.5, label='Mag from -g-(+i)')
     plt.plot(r, d, 'mo', alpha=0.5, label='Mag from r > 158')
     plt.xlim(0,4)
-    plt.ylim(0,161)
+    plt.ylim(0,180)
     plt.xlabel('G-I Magnitude')
     plt.ylabel('Half-Light Radius (arcsec)')
     plt.savefig('g_minus_i_vs_radius.png')
@@ -134,12 +155,14 @@ fig4= plt.figure(4)
 if __name__ == '__main__':
     data = pyf.open("../data/nsa-short.fits.gz")[1].data
     y=data.field('SERSICFLUX')
+    z = data.field('SERSIC_TH50')
     good=np.array([True for x in data.field('RA')])
     indx1=np.where(y[:,4] < 0)
     good[indx1]=False
     indx2=np.where(y[:,2] < 0)
     good[indx2]=False
-    indx3=np.where(y[:,4] > 0)
+    indx3=np.where(z > 158)
+    good[indx3]=False
     def Mag(y): 
         return 22.5-2.5*np.log10(abs(y)) 
     i=Mag(y[:,4][good]) #mag of positive I values
@@ -153,15 +176,20 @@ if __name__ == '__main__':
     l=Mag(y[:,4][indx2]) #mag of I values corresponding to neg G values
     
     m=Mag(y[:,2][indx1]) #mag of G values corresponding to neg I values
-    a=y[good]
-    b=y[indx1]
-    c=y[indx2]
+    
+    p=Mag(y[:,4][indx3]) # mag of I values that correspond to r > 158 
+    
+    q=Mag(y[:,2][indx3]) # mag of G values that correspond to r > 158
+    
     h=g-i
     n=m-j
     o=k-l
+    r=q-p
+    
     plt.plot(i,h,'ko', alpha=0.5, label='Mag from +g-(+i)')  
     plt.plot(j,n, 'mo', alpha=0.5, label='Mag from +g-(-i)')
     plt.plot(l,o, 'mo', alpha=0.5, label='Mag from -g-(+i)')
+    plt.plot(p,r, 'mo', alpha=0.5, label='Mag from r >158')
     plt.xlabel('i-magnitude')
     plt.ylabel('g-i color')
     plt.xlim(6,21)
@@ -169,8 +197,7 @@ if __name__ == '__main__':
     plt.savefig('imag_vs_g_minus_i_color.png')
     plt.legend()
  
- 
-    
+     
     
 plt.show()  
 
