99226a27a113127a39a758c24dda39ce9d912a7a
Ekta Patel <ep1091@nyu.edu>
2012-05-22 15:34:47 -0400

    now includes URL for image and URL for parent directory

--- py/finding_comments_ep.py
+++ py/finding_comments_ep.py
@@ -45,17 +45,40 @@ for theid in y[good==False]:
         print 'comment:',comment[i]
         declination=[s for s in xrange(len(b)) if nsaid[i] == y[s]]
         for s in declination:
-            if b[s] > 0: print 'http://sdss.physics.nyu.edu/mblanton/v0/detect/v0_1/%sh/p%02d/%s/%s.jpg' % (w[s][1:3],(int(w[s][11:13])/2)*2,w[s],w[s])
-            else: print 'http://sdss.physics.nyu.edu/mblanton/v0/detect/v0_1/%sh/m%02d/%s/%s.jpg' % (w[s][1:3],(int(w[s][11:13])/2)*2,w[s],w[s])
-            print #space#																						
+            if b[s] > 0: 
+            	print 'http://sdss.physics.nyu.edu/mblanton/v0/detect/v0_1/%sh/p%02d/%s/%s.jpg' % (w[s][1:3],(int(w[s][11:13])/2)*2,w[s],w[s])
+            	print 'http://sdss.physics.nyu.edu/mblanton/v0/detect/v0_1/%sh/p%02d/%s/atlases/%s/' % (w[s][1:3],(int(w[s][11:13])/2)*2,w[s],c[s])
+            else: 
+            	print 'http://sdss.physics.nyu.edu/mblanton/v0/detect/v0_1/%sh/m%02d/%s/%s.jpg' % (w[s][1:3],(int(w[s][11:13])/2)*2,w[s],w[s])
+                print 'http://sdss.physics.nyu.edu/mblanton/v0/detect/v0_1/%sh/m%02d/%s/atlases/%s/' % (w[s][1:3],(int(w[s][11:13])/2)*2,w[s],c[s])
+        print #space#																						
        		
 nocom = [t for t in xrange(len(y[good==False])) if y[good==False][t] not in nsaid]
 for t in nocom:    
-    print 'NSAID',y[good==False][t]
-    print 'IAU:',w[good==False][t]
-    print 'flux:',x[good==False][t]
-    print 'radius:',z[good==False][t]
-    print 'comment: NONE '
-    if b[good==False][t] > 0: print 'http://sdss.physics.nyu.edu/mblanton/v0/detect/v0_1/%sh/p%02d/%s/%s.jpg' % (w[good==False][t][1:3],(int(w[good==False][t][11:13])/2)*2,w[good==False][t],w[good==False][t])
-    else: print 'http://sdss.physics.nyu.edu/mblanton/v0/detect/v0_1/%sh/m%02d/%s/%s.jpg' % (w[good==False][t][1:3],(int(w[good==False][t][11:13])/2)*2,w[good==False][t],w[good==False][t])
-    print #space#
+	print 'NSAID',y[good==False][t]
+	print 'IAU:',w[good==False][t]
+	print 'flux:',x[good==False][t]
+	print 'radius:',z[good==False][t]
+	print 'comment: NONE '    
+	if b[good==False][t] > 0: 
+    		print 'http://sdss.physics.nyu.edu/mblanton/v0/detect/v0_1/%sh/p%02d/%s/%s.jpg' % (w[good==False][t][1:3],(int(w[good==False][t][11:13])/2)*2,w[good==False][t],w[good==False][t])
+    		print 'http://sdss.physics.nyu.edu/mblanton/v0/detect/v0_1/%sh/p%02d/%s/atlases/%s/' % (w[good==False][t][1:3],(int(w[good==False][t][11:13])/2)*2,w[good==False][t],c[good==False][t])
+	
+	else: 
+			print 'http://sdss.physics.nyu.edu/mblanton/v0/detect/v0_1/%sh/m%02d/%s/%s.jpg' % (w[good==False][t][1:3],(int(w[good==False][t][11:13])/2)*2,w[good==False][t],w[good==False][t])
+			print 'http://sdss.physics.nyu.edu/mblanton/v0/detect/v0_1/%sh/m%02d/%s/atlases/%s/' % (w[good==False][t][1:3],(int(w[good==False][t][11:13])/2)*2,w[good==False][t],c[good==False][t])
+	print #space#
+
+
+
+    
+
+    
+        
+
+    
+    
+ 
+
+#u g r i z
+    
