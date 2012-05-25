0e4ae42e5f2c5c084a0ef2165c22235a1954ff1f
Ekta Patel <ep1091@nyu.edu>
2012-05-24 22:57:54 -0400

    index updates in flux

--- py/select_and_caption.py
+++ py/select_and_caption.py
@@ -71,9 +71,9 @@ if __name__ == '__main__':
     y4=b[indx3]
     
     #i-magnitudes    
-    i1=Mag2(y[:,4][good])
-    i2=Mag1(y[:,4][indx1])
-    i3=Mag1(y[:,4][indx3])
+    i1=Mag2(y[:,3][good])
+    i2=Mag1(y[:,3][indx1])
+    i3=Mag1(y[:,3][indx3])
     
     #radii
     a=z[good]
@@ -88,26 +88,26 @@ if __name__ == '__main__':
     c4=concentration(p90[indx3],p50[indx3])
     
     #g & i magnitudes
-    i=Mag1(y[:,4][good]) #mag of positive I values
-    g=Mag1(y[:,2][good]) #mag of positive G values
-    j=Mag1(y[:,4][indx1]) #mag of negative I values
-    k=Mag1(y[:,2][indx2]) #mag of negative G values 
-    l=Mag1(y[:,4][indx2]) #mag of I values corresponding to neg G values
-    m=Mag1(y[:,2][indx1]) #mag of G values corresponding to neg I values
-    p=Mag1(y[:,4][indx3]) # mag of I values that correspond to r > 158 
-    q=Mag1(y[:,2][indx3]) # mag of G values that correspond to r > 158
+    i=Mag1(y[:,3][good]) #mag of positive I values
+    g=Mag1(y[:,1][good]) #mag of positive G values
+    j=Mag1(y[:,3][indx1]) #mag of negative I values
+    k=Mag1(y[:,1][indx2]) #mag of negative G values 
+    l=Mag1(y[:,3][indx2]) #mag of I values corresponding to neg G values
+    m=Mag1(y[:,1][indx1]) #mag of G values corresponding to neg I values
+    p=Mag1(y[:,3][indx3]) # mag of I values that correspond to r > 158 
+    q=Mag1(y[:,1][indx3]) # mag of G values that correspond to r > 158
     
-    #colors
-    h=g-i #all positive colors
-    n1=m-j #+G-(-I)
-    o=k-l #-G-(+I)
-    r=q-p # bad colors from r > 158 
-    
-    #i-surface brightnesses       
-    sb=i+SB(z[good]) #i-sb from good values
-    sb1=p+SB(z[indx3]) #i-sb from bad r values
-    sb2=j+SB(z[indx1])#i-sb from negative i values 
-    sb3=l+SB(z[indx2])#i-sb from negative g values
+	#colors
+	h=g-i #all positive colors
+	n1=m-j #+G-(-I)
+	o=k-l #-G-(+I)
+	r=q-p # bad colors from r > 158 
+	
+	#i-surface brightnesses       
+	sb=i+SB(z[good]) #i-sb from good values
+	sb1=p+SB(z[indx3]) #i-sb from bad r values
+	sb2=j+SB(z[indx1])#i-sb from negative i values 
+	sb3=l+SB(z[indx2])#i-sb from negative g values
 
 
 #scatter plot of RA vs. DEC 
@@ -133,13 +133,13 @@ plt.xlim(7,23)
 plt.ylim(0,180)
 plt.savefig('radius_imag.png')
 
-#G-I magnitude vs. radius
+#G-I color vs. radius
 fig3 = plt.figure(3)
 plt.plot(n1,b, 'm.', alpha=0.5, label='Mag from +g-(-i)')
 plt.plot(o,c, 'm.', alpha=0.5, label='Mag from -g-(+i)')
 plt.plot(r, d, 'm.', alpha=0.5, label='Mag from r > 158')
 plt.plot(h,a,'k.', alpha=0.5, label='Mag from +g-(+i)')
-plt.xlim(0,4)
+plt.xlim(0,6)
 plt.ylim(0,180)
 plt.xlabel(r"$g-i$ color (mag)")
 plt.ylabel('Half-Light Radius (arcsec)')
@@ -153,8 +153,8 @@ plt.plot(l,o, 'm.', alpha=0.5, label='Mag from -g-(+i)')
 plt.plot(i,h,'k.', alpha=0.5, label='Mag from +g-(+i)')  
 plt.xlabel(r"$i$ (mag)")
 plt.ylabel(r"$g-i$ color (mag)")
-plt.xlim(6,21)
-plt.ylim(0,4)
+plt.xlim(8,21)
+plt.ylim(0,6)
 plt.legend()
 plt.savefig('imag_vs_g_minus_i_color.png')
  
@@ -164,25 +164,29 @@ plt.plot(sb1, r, 'm.', alpha=0.5, label='sb of r > 158')
 plt.plot(sb2,n1,'m.',alpha=0.5, label='sb of neg i-flux')
 plt.plot(sb3, o, 'm.', alpha=0.5, label='sb of neg g-flux')
 plt.plot(sb, h, 'k.', alpha=0.5, label='sb of good values')
-plt.ylim(0,4)
-plt.xlim(14,27)
+plt.ylim(0,6)
+plt.xlim(16,28)
 plt.xlabel(r"$i$ Surface Brightness(mag/arcsec$^2$)")
 plt.ylabel(r"$g-i$ color (mag)")
-plt.axhline(y=1.33682, color='r',label='1.33682')
-plt.axhline(y=1.80399, color='r',label='1.80399')
-plt.axhline(y=2.50706, color='r',label='2.50706')
-plt.axvline(x=19.5282, ymin=0, ymax=.334205, color='r')
-plt.axvline(x=21.0245, ymin=0, ymax=.334205, color='r')
-plt.axvline(x=22.1049, ymin=0, ymax=.334205, color='r')
-plt.axvline(x=18.8572, ymin=.334205, ymax=.4509975, color='r')
-plt.axvline(x=19.975, ymin=.334205, ymax=.4509975, color='r')
-plt.axvline(x=21.6505, ymin=.334205, ymax=.4509975, color='r')
-plt.axvline(x=17.7814, ymin=.4509975, ymax=.626765,color='r')
-plt.axvline(x=18.7856, ymin=.4509975, ymax=.626765,color='r')
-plt.axvline(x=19.8334, ymin=.4509975, ymax=.626765,color='r')
-plt.axvline(x=17.5609, ymin=.626765, ymax=4, color='r')
-plt.axvline(x=18.5066, ymin=.626765, ymax=4, color='r')
-plt.axvline(x=19.2279, ymin=.626765,ymax=4, color='r')
+plt.axhline(y=2.12026, color='r',label='2.12026')
+plt.axhline(y=2.95937, color='r',label='2.95937')
+plt.axhline(y=4.16176, color='r',label='4.16176')
+
+plt.axvline(x=19.5542, ymin=0, ymax=.35338, color='r')
+plt.axvline(x=20.6786, ymin=0, ymax=.35338, color='r')
+plt.axvline(x=22.0141, ymin=0, ymax=.35338, color='r')
+
+plt.axvline(x=19.0024, ymin=.35338, ymax=.49323, color='r')
+plt.axvline(x=20.2374, ymin=.35338, ymax=.49323, color='r')
+plt.axvline(x=22.0168, ymin=.35338, ymax=.49323, color='r')
+
+plt.axvline(x=18.9858, ymin=.49323, ymax=.69363,color='r')
+plt.axvline(x=19.993, ymin=.49323, ymax=.69363,color='r')
+plt.axvline(x=21.1327, ymin=.49323, ymax=.69363,color='r')
+
+plt.axvline(x=18.3378, ymin=.69363, ymax=6, color='r')
+plt.axvline(x=19.3626, ymin=.69363, ymax=6, color='r')
+plt.axvline(x=20.3118, ymin=.69363,ymax=6, color='r')
 plt.savefig('i_surfacebrightness_vs_color.png')
 
 #radius vs. radius concentration
