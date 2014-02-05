dd89b5ba914cd9ad93fc881300a8f240358ab7be
David W. Hogg <dwhogg@gmail.com>
2014-02-04 21:33:38 -0500

    OMG plate-building works

--- py/make_plates.py
+++ py/make_plates.py
@@ -16,7 +16,6 @@ def make_one_plate(filelist, nx=2400):
     bugs:
     - Very brittle; assumes input is `PIL RGB` format.
     - Can only make square plates with square subimages.
-    - Written but not tested.
     '''
     nim = len(filelist)
     # get image grid size check that nim is a square
@@ -26,7 +25,7 @@ def make_one_plate(filelist, nx=2400):
     nxim = nx / nimx # does this produce an int?
     assert nx == nimx * nxim
     # make plate canvas
-    platedata = np.zeros((nx, nx, 3)).astype(int)
+    platedata = np.zeros((nx, nx, 3)).astype(np.uint8)
     # loop over file names
     for ii, fn in enumerate(filelist):
         iix = ii % nimx
@@ -37,23 +36,32 @@ def make_one_plate(filelist, nx=2400):
         assert thisim.size[1] >= nxim
         assert 2 * (thisim.size[0] / 2) == thisim.size[0]
         assert 2 * (thisim.size[1] / 2) == thisim.size[1]
-        thisdata = np.array(thisim.getdata()).reshape(thisim.size[0], thisim.size[1], 3) # MAGIC 3
+        thisdata = np.asarray(thisim).copy()
+        print thisdata.shape, np.min(thisdata), np.max(thisdata)
         # compute input and output pixel-offset locations
-        x1 = iix * nimx
-        y1 = iiy * nimx
-        x2 = (thisim.size[0] - nimx) / 2
-        y2 = (thisim.size[1] - nimx) / 2
-        print ii, iix, iiy, nxim, fn, thisim.size[0], x2
+        x1 = iix * nxim
+        y1 = iiy * nxim
+        x2 = (thisim.size[0] - nxim) / 2
+        y2 = (thisim.size[1] - nxim) / 2
+        print ii, iix, iiy, nxim, fn, thisim.size[0], x2, y2
         # attach white border to input data
-        thisdata[x2,:,:] = 255 # MAGIC 255
-        thisdata[x2 + nimx - 1,:,:] = 255 # MAGIC 255
-        thisdata[:,y2,:] = 255 # MAGIC 255
-        thisdata[:,y2 + nimx - 1,:] = 255 # MAGIC 255
+        # note x <-> y issues
+        thisdata[y2,:,:] = 255 # MAGIC 255
+        thisdata[y2 + nxim - 1,:,:] = 255 # MAGIC 255
+        thisdata[:,x2,:] = 255 # MAGIC 255
+        thisdata[:,x2 + nxim - 1,:] = 255 # MAGIC 255
         # merge new data into plate canvas
-        platedata[x1:x1 + nimx,y1:y1 + nimx,:] = thisdata[x2:x2 + nimx,y2:y2 + nimx,:]
+        # note x <-> y issues
+        platedata[y1:y1 + nxim,x1:x1 + nxim,:] = thisdata[y2:y2 + nxim,x2:x2 + nxim,:]
     # make output image object
-    plate = im.new("RGB", (nx, nx), "white")
+    print platedata.shape, np.min(platedata), np.max(platedata)
+    plate = im.fromarray(platedata)
     return plate
 
 if __name__ == "__main__":
-    print make_one_plate(["foo", "bar", "hello", "whatevs"])
+    fns = ["./test_data/A_0045-10_MCG_-2_3_16_irg.jpg",
+           "./test_data/NGC_151_MCG_-2_2_54_IRAS_00315-0958_irg.jpg",
+           "./test_data/NGC_173_UGC_369_IRAS_00346+0140_irg.jpg",
+           "./test_data/NGC_7814_UGC_8_irg.jpg",
+           ]
+    make_one_plate([fns[ii] for ii in (1,2,3,0,1,2,3,3,3,0,1,2,3,0,1,2)], nx=2400).save("foo.jpg")
