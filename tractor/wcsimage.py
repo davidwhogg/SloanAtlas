fd3c914c53e91cbaf4e08966b58cd9dfbb9da3bb
Ekta Patel <ep1091@nyu.edu>
2014-02-28 20:22:54 +0000

    measuring geometric mean

--- tractor/wcsimage.py
+++ tractor/wcsimage.py
@@ -25,7 +25,7 @@ from tractor.fitpsf import em_init_params
 
 matplotlib.rcParams.update({'font.size': 10})
 
-def wcsimage(name, iauname, direc='RC3_Output'):
+def wcsimage(name, iauname, mean=False, direc='RC3_Output'):
     CG = unpickle_from_file("%s/%s.pickle" %(direc,name))
     # ra,dec = CG.getPosition()
     # maxradius=max(CG.shapeExp.re,CG.shapeDev.re)
@@ -88,7 +88,6 @@ def wcsimage(name, iauname, direc='RC3_Output'):
     xlim = plt.xlim()
     ylim = plt.ylim()
     
-
     plt.subplot(131)
     data = f[0].data
     print np.min(data)
@@ -102,15 +101,68 @@ def wcsimage(name, iauname, direc='RC3_Output'):
     plt.subplot(133)
     vmin = -0.0625 * np.max(mimgs[3])
     vmax = -vmin
+    model = mimgs[3]
     plt.imshow(data-mimgs[3], origin ='lower',cmap=matplotlib.cm.Greys, vmin=vmin, vmax=vmax)
     plt.xlim(4*len(mimgs[3])/9, 5*len(mimgs[3])/9)
     plt.ylim(4*len(mimgs[3])/9, 5*len(mimgs[3])/9)
     plt.title('difference')
     plt.tight_layout()
     plt.savefig('%s_data_wcs.pdf' %(name))
+    
+
+    if mean:
+        
+        r50s = []
+        r90s = []
+        concs = []
+        h50s =[]
+        h90s = [] 
+        expr50 = CG.shapeExp.re * np.sqrt(CG.shapeExp.ab)
+        devr50 = CG.shapeDev.re * np.sqrt(CG.shapeDev.ab)
+        yg, xg = np.meshgrid(np.arange(imageh) - crpix2, np.arange(imagew) - crpix1)
+        r2g = xg ** 2 + yg ** 2
+        rlist_pix = np.exp(np.linspace(0.,np.log(0.5*imageh),64))
+        rlist_arcsec = rlist_pix * pixscale * 3600.
+
+        betas = np.arange(0.01,0.99,0.1)
+        print betas
+        model = np.array(model)
+        for beta in betas:
+            dm = (data ** beta) * (model ** (1-beta))
+            image = dm
+
+            # make variance tensor out of image; get principal eigenvector
+            foo = np.sum(image * xg * yg)
+            V = np.array([[np.sum(image * xg * xg), foo],
+                          [foo, np.sum(image * yg * yg)]])
+            u, v = np.linalg.eig(V) 
+            #print v[np.argmax(u)], v[:,np.argmax(u)], u
+            principal_vector = v[:,np.argmax(u)].reshape((2,)) 
+            assert (np.dot(principal_vector, principal_vector) - 1.) < 1e-8
+
+            # do rectangular integrals on "diagonals"
+            rdotv = xg * principal_vector[0] + yg * principal_vector[1]
+            hlist = [np.sum(image[np.abs(rdotv) < r]) for r in rlist_pix]
+            hlist /= hlist[-1]
+            h50, h90 = np.interp([0.5,0.9],hlist, rlist_arcsec)
+            h50s.append(h50)
+            h90s.append(h90)
+            print h50, h90, 'h50 h90'
+
+            # do circular integrals
+            plist = [np.sum(image[r2g < (r * r)]) for r in rlist_pix]
+            plist /= plist[-1]
+            r50, r90 = np.interp([0.5, 0.9], plist, rlist_arcsec)
+            print r50,r90
+            conc = r90/r50
+            r50s.append(r50)
+            r90s.append(r90)
+            concs.append(conc)
+            print r50, r90, 'r50','r90'
 
     return 'done'
 
+    
 if __name__ == "__main__":
-    wcsimage('NGC_4736','J125053.00+410712.4')
-    wcsimage('NGC_3377','J104742.33+135909.3')
+    wcsimage('NGC_4736','J125053.00+410712.4', mean=True)
+    wcsimage('NGC_3377','J104742.33+135909.3', mean=True)
