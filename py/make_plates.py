13ec24fca609ed609f408da9839014b0df36c29b
davidwhogg <david.hogg@nyu.edu>
2014-03-18 07:18:12 -0400

    merge in Ekta plotting

--- py/make_plates.py
+++ py/make_plates.py
@@ -1,10 +1,21 @@
-# This file is part of the Sloan Atlas project.
-# Copyright 2014 David W. Hogg (NYU).
+"""
+This file is part of the Sloan Atlas project.
+Copyright 2014 David W. Hogg (NYU) & Ekta Patel (NYU).
+
+bugs:
+-----
+* Many things hard coded, brittle!
+* Assumptions about directory structure.
+"""
 
 import os
 import numpy as np
 import pyfits as pf # works on broiler
 from PIL import Image as im
+import matplotlib
+matplotlib.use('Agg')
+matplotlib.rc('text', usetex=True)
+import matplotlib.pyplot as plt
 
 FIDUCIAL = 5. * 60. # MAGIC number in arcsec
 
@@ -98,7 +109,68 @@ def move_files_from_remote_server(names):
         fns = np.append(fns, fn)
     return fns
 
-def make_one_quantile_of_plates(prefix, names, sizes, captions):
+def make_one_caption_figure(names, fn, data):
+    """
+    bugs:
+    * Needs more information in this comment header.
+    * Wrong calling sequence given call (below)
+    """
+    # massage data
+    mags=data.field("CG_TOTMAGS")
+    extinction=data.field("CG_EXTINCTION")
+    c=data.field("CG_CONC")
+    c=c[:,3]
+    mu50=data.field("CG_I-SB")
+    u=mags[:,0]-extinction[:,0]
+    g=mags[:,1]-extinction[:,1]
+    r=mags[:,2]-extinction[:,2]
+    i=mags[:,3]-extinction[:,3]
+    z=mags[:,4]-extinction[:,4]
+
+    plt.figure(figsize=(6,6))
+    plt.subplots_adjust(hspace=0.08)
+
+    ax=plt.subplot(211)
+    ax.xaxis.set_visible(True)
+    plt.setp(ax.get_xticklabels(),visible=False)
+    gmi = g-i
+    plt.plot(gmi, mu50, "o",
+             color="0.6", alpha=0.5, mew=0, markeredgecolor="none")
+    plt.xlim(0.1,1.4)
+    plt.ylim(16,26)
+    plt.ylabel(r"$\mu_{50,i}$")
+    for ii, name in enumerage(names):
+        label = ("%d" % ii).trim()
+        jj = np.where(data["NAME"] == name)[0]
+        print "make_one_caption_figure(): found objects with indices and quantiles:", jj, data[jj]["QUANTILE"]
+        assert len(jj) == 1
+        jj = jj[0]
+        plt.plot(gmi[jj], mu50[jj], "o",
+                 color="black", ms=5, markeredgecolor="black")
+        plt.annotate(labels[jj], xy=(gmi[jj], mu50[jj]), xytext=(gmi[jj], mu50[jj]+0.15), # magic
+                     xycoords="data",textcoords="data",color="black",size="large")
+    
+    plt.subplot(212)
+    plt.plot(gmi, c, "o",
+             color="0.6", alpha=0.5, mew=0, markeredgecolor="none") # synchronized with above
+    plt.xlim(0.1,1.4)
+    plt.ylim(2,5)
+    plt.xlabel(r"$g-i$ (mag)")
+    plt.ylabel(r"$C_i$ (mag)")
+    for ii, name in enumerage(names):
+        label = ("%d" % ii).trim()
+        jj = np.where(data["NAME"] == name)[0]
+        print "make_one_caption_figure(): found objects with indices and quantiles:", jj, data[jj]["QUANTILE"]
+        assert len(jj) == 1
+        jj = jj[0]
+        plt.plot(gmi[jj], c[jj], "o",
+                 color="black", ms=5, markeredgecolor="black") # synchronized with above
+        plt.annotate(labels[jj], xy=(gmi[jj], c[jj]), xytext=(gmi[jj], c[jj]+0.15), # magic
+                     xycoords="data",textcoords="data",color="black",size="large") # synchronized with above
+    plt.savefig(fn)
+    return None
+
+def make_one_quantile_of_plates(prefix, names, sizes, captions, tabdata):
     """
     inputs:
     - `prefix` - string for plate naming
@@ -125,7 +197,6 @@ def make_one_quantile_of_plates(prefix, names, sizes, captions):
     sizes = sizes[II]
     captions = captions[II]
     fns = fns[II]
-    # captions = [cc + " " + ff for (cc, ff) in zip(captions, fns)]
     print captions
     nim = len(names)
     assert len(sizes) == nim
@@ -134,7 +205,8 @@ def make_one_quantile_of_plates(prefix, names, sizes, captions):
     while listindex < nim:
         thisprefix = "%s_%02d" % (prefix, platenum)
         outimgfn = "%s.jpg" % (thisprefix, )
-        outtxtfn = "%s.tex" % (thisprefix, )
+        outtxtfn = "%s_caption.tex" % (thisprefix, )
+        outfigfn = "%s_caption.png" % (thisprefix, )
         nimx = int(np.floor(FIDUCIAL / sizes[listindex]))
         if nimx < 1: nimx = 1
         if nimx > 4: nimx = 4
@@ -145,7 +217,8 @@ def make_one_quantile_of_plates(prefix, names, sizes, captions):
         else:
             print "making %s" % (outimgfn, )
             make_one_plate(fns[listindex:listindex + nimx * nimx]).save(outimgfn)
-            fd = open(outtxtfn, "w") # wrong syntax?
+            make_one_caption_figure(names[listindex:listindex + nimx * nimx], outfigfn, tabdata)
+            fd = open(outtxtfn, "w")
             for ii in range(nimx * nimx):
                 fd.write("%0d. --- %s\n" % (ii, captions[listindex + ii]))
                 if ii < (nimx * nimx - 1):
@@ -170,7 +243,7 @@ def remove_all_exceptions(data):
     """
     # remove bad data
     bad = np.zeros(len(data))
-    bad[np.where(data.NAME == "UGC 11891")] = 1 # looks red / wrong?
+    # bad[np.where(data.NAME == "UGC 11891")] = 1 # looks red / wrong?
     bad[np.where(data.NAME == "A 1246-09")] = 1 # missing imaging data
     bad[np.where(data.NAME == "UGC 6773")] = 1 # bright star messing it up?
     bad[np.where(data.NAME == "NGC 3769A")] = 1 # bad deblend with B
@@ -181,27 +254,27 @@ def remove_all_exceptions(data):
     bad[np.where(data.NAME == "UGC 5245")] = 1 # missing data
     bad[np.where(data.NAME == "A 1420+45")] = 1 # missing data
 
-    bad[np.where(data.NAME == "NGC 2283")] = 1 # bad calibration or highly reddened?
-    bad[np.where(data.NAME == "UGC 11861")] = 1 # bad calibration or highly reddened?
+    # bad[np.where(data.NAME == "NGC 2283")] = 1 # bad calibration or highly reddened?
+    # bad[np.where(data.NAME == "UGC 11861")] = 1 # bad calibration or highly reddened?
     bad[np.where(data.NAME == "NGC 7741")] = 1 # missing imaging data
-    bad[np.where(data.NAME == "UGC 11818")] = 1 # bad calibration or highly reddened?
+    # bad[np.where(data.NAME == "UGC 11818")] = 1 # bad calibration or highly reddened?
     bad[np.where(data.NAME == "NSA ID 131288")] = 1 # totally wrong and red and small
-    bad[np.where(data.NAME == "UGC 3433")] = 1 # bad calibration or highly reddened?
+    # bad[np.where(data.NAME == "UGC 3433")] = 1 # bad calibration or highly reddened?
 
     bad[np.where(data.NAME == "A 2233-03")] = 1 # missing imaging data
-    bad[np.where(data.NAME == "UGC 2728")] = 1 # bad calibration or highly reddened?
+    # bad[np.where(data.NAME == "UGC 2728")] = 1 # bad calibration or highly reddened?
     bad[np.where(data.NAME == "UGC 1230")] = 1 # missing imaging data
     bad[np.where(data.NAME == "UGC 6660")] = 1 # what the fuck?
     bad[np.where(data.NAME == "NGC 205")] = 1 # missing imaging data
 
     bad[np.where(data.NAME == "NGC 6070")] = 1 # missing imaging data
     bad[np.where(data.NAME == "NGC 7678")] = 1 # missing imaging data
-    bad[np.where(data.NAME == "UGC 2358")] = 1 # bad calibration or highly reddened?
+    # bad[np.where(data.NAME == "UGC 2358")] = 1 # bad calibration or highly reddened?
     bad[np.where(data.NAME == "NGC 855")] = 1 # missing imaging data
     bad[np.where(data.NAME == "NGC 7497")] = 1 # missing imaging data
     bad[np.where(data.NAME == "NSA ID 133245")] = 1 # bright star messing it up?
 
-    bad[np.where(data.NAME == "UGC 2411")] = 1 # bad calibration or highly reddened?
+    # bad[np.where(data.NAME == "UGC 2411")] = 1 # bad calibration or highly reddened?
     bad[np.where(data.NAME == "A 0102-06")] = 1 # missing imaging data
     bad[np.where(data.NAME == "UGC 2468")] = 1 # wrong size or galaxy cluster?
     bad[np.where(data.NAME == "NSA ID 44968")] = 1 # needs to be rerun in deblend mode
@@ -255,7 +328,7 @@ def make_all_plates(catalogfn):
         II = np.where(np.abs(tabdata.QUANTILE - quantile) < tiny)[0]
         II = II[(np.argsort(plotsizes[II]))[::-1]]
         print tabdata.NAME[II], tabdata.CG_H90S[II,fb], plotsizes[II]
-        make_one_quantile_of_plates(prefix, tabdata.NAME[II], plotsizes[II], captions[II])
+        make_one_quantile_of_plates(prefix, tabdata.NAME[II], plotsizes[II], captions[II], tabdata)
     return None
 
 if __name__ == "__main__":
